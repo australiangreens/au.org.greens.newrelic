b3d8a92ae64874ee73985a988430504aa814f4da
Seamus Lee <seamuslee001@gmail.com>
2024-02-24 09:55:59 +1100

    Run Civix upgrade

--- newrelic.php
+++ newrelic.php
@@ -24,59 +24,12 @@ function newrelic_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function newrelic_civicrm_postInstall() {
-  _newrelic_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function newrelic_civicrm_uninstall() {
-  _newrelic_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function newrelic_civicrm_enable() {
   _newrelic_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function newrelic_civicrm_disable() {
-  _newrelic_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function newrelic_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _newrelic_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function newrelic_civicrm_entityTypes(&$entityTypes) {
-  _newrelic_civix_civicrm_entityTypes($entityTypes);
 }
 
 /**
